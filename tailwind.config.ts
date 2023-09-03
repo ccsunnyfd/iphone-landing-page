1b48fcdaf6c3bac5dddefe4ddbe18ec46639ab2f
ccsunnyfd <chzhyfd@126.com>
2023-09-03 11:10:39 +0800

    [feature]LoadingScreen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,38 @@ const config: Config = {
         glow: {
           '0%, 100%': { boxShadow: '1px 1px 10px #ffffff' },
           '50%': { boxShadow: '2px 2px 25px #ffffff' },
+        },
+        changeBackground: {
+          '0%': {
+            backgroundColor: '#9bb5ce',
+            color: '#a50011',
+            fill: '#a50011',
+          },
+          '20%': {
+            backgroundColor: '#f9e5c9',
+            color: '#574f6f',
+            fill: '#574f6f',
+          },
+          '40%': {
+            backgroundColor: '#505f4e',
+            color: '#9bb5ce',
+            fill: '#9bb5ce',
+          },
+          '60%': {
+            backgroundColor: '#574f6f',
+            color: '#9bb5ce',
+            fill: '#9bb5ce',
+          },
+          '80%': {
+            backgroundColor: '#a50011',
+            color: '#f9e5c9',
+            fill: '#f9e5c9',
+          },
+          '100%': {
+            backgroundColor: '#9bb5ce',
+            color: '#505f4e',
+            fill: '#505f4e',
+          },
         }
       },
       backgroundImage: {
